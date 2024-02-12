c4750bd87003c0b8267757e16127f7d56c05bade
FehmiTahsinDemirkan <fehmitahsindemirkan@gmail.com>
2024-02-13 00:34:56 +0300

    Dictionary lessons

--- Lists/dictionary.py
+++ Lists/dictionary.py
@@ -1,4 +1,9 @@
+# Dictionary
+# Dictionaries are used to store data values in key:value pairs.
+#
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
 # key - value
+
 
 # 41 => Kocaeli
 # 34 => İstanbul

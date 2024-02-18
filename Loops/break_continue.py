cf16a622d0a56f2ea0cd55158732fa07fbcac661
FehmiTahsinDemirkan <fehmitahsindemirkan@gmail.com>
2024-02-18 23:38:08 +0300

    Loops part 1

---
+++ Loops/break_continue.py
@@ -0,0 +1,30 @@
+# isim = "Fehmi Tahsin"
+
+# for harf in isim:
+#      if (harf == "s"):
+#          break
+#      print(harf)
+
+# i = 0
+# while (i < 5):
+#     i += 1
+#     if (i == 3):
+#         continue
+#     print(i)
+
+# print('döngü bitti.')
+
+# 1-100 arasındaki çift sayılar toplamı.
+
+i = 0
+toplam = 0
+
+while (i <= 100):
+    i += 1
+    if (i % 2 == 1):
+        continue
+    toplam += i
+
+print(f"toplam: {toplam}")
+
+print("\U0001f600")

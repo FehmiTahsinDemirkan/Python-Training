9607605ee0ffa593748be7397c1517dd143518fc
FehmiTahsinDemirkan <fehmitahsindemirkan@gmail.com>
2024-02-18 19:35:47 +0300

    Conditional Statements Finished

---
+++ Conditional_Statements/if_else.py
@@ -0,0 +1,10 @@
+username = "Fehmi"
+password = "12345"
+
+if (username == "Fehmi"):
+    if (password == "1234"):
+        print('Uygulamaya hoş geldiniz.')
+    else:
+        print("parola yanlış")
+else:
+    print("username yanlış")
